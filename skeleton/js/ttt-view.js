2ff6444835ce7ca36a191df3d74ce5f6c0549995
Colin Jackson <colin.s.jackson90@gmail.com>
2014-10-15 19:19:27 -0400

    Merge CSS with jQuery-generated HTML elements

--- skeleton/js/ttt-view.js
+++ skeleton/js/ttt-view.js
@@ -4,6 +4,8 @@
   }
 
   var View = TTT.View = function (game, $el) {
+    this.$el = $el;
+    this.setupBoard();
   };
 
   View.prototype.bindEvents = function () {
@@ -13,5 +15,29 @@
   };
 
   View.prototype.setupBoard = function () {
+    var winHeight = $(window).height();
+    var winWidth = $(window).width();
+    var length = (winHeight < winWidth ? winHeight - 52 : winWidth - 52);
+    this.$el.width(length).height(length);
+
+    for (var i = 0; i < 3; i++) {
+      var $rowDiv = $("<div></div>");
+      $rowDiv.height((length / 3) - 4);
+      $rowDiv.addClass("row");
+
+      for (var j = 0; j < 3; j++) {
+        var $cellDiv = $("<div></div>");
+        $cellDiv.width((length / 3) - 4);
+        $cellDiv.addClass("cell");
+
+        var $squareDiv = $("<div></div>");
+        $squareDiv.addClass("square");
+
+        $cellDiv.append($squareDiv);
+        $rowDiv.append($cellDiv);
+      }
+
+      this.$el.append($rowDiv);
+    }
   };
 })();
