1b90322a2441e10822724b73823875f609c473ac
Colin Jackson <colin.s.jackson90@gmail.com>
2014-10-15 21:47:18 -0400

    Merge model logic with HTML/CSS/jQuery browser view

--- skeleton/js/ttt-view.js
+++ skeleton/js/ttt-view.js
@@ -5,13 +5,49 @@
 
   var View = TTT.View = function (game, $el) {
     this.$el = $el;
+    this.game = game;
+
     this.setupBoard();
+    this.bindEvents();
   };
 
   View.prototype.bindEvents = function () {
+    $(".square").on("click", function () {
+      var $square = $(event.currentTarget);
+      $square.addClass("square-occupied");
+
+      this.makeMove($square);
+    }.bind(this));
   };
 
   View.prototype.makeMove = function ($square) {
+    var pos = $square.data("pos");
+    var mark = this.game.currentPlayer;
+
+    this.game.playMove(pos);
+    $square.append(mark);
+
+    if (this.checkForGameOver()) {
+      this.game = new TTT.Game();
+      this.$el.empty();
+      this.setupBoard();
+      this.bindEvents();
+    }
+  };
+
+  View.prototype.checkForGameOver = function () {
+    if (!this.game.isOver()) {
+      return false;
+    }
+
+    var winner = this.game.winner();
+    if (winner) {
+      window.alert(winner + " wins!");
+    } else {
+      window.alert("It's a draw...");
+    }
+
+    return true;
   };
 
   View.prototype.setupBoard = function () {
@@ -21,23 +57,30 @@
     this.$el.width(length).height(length);
 
     for (var i = 0; i < 3; i++) {
-      var $rowDiv = $("<div></div>");
-      $rowDiv.height((length / 3) - 4);
-      $rowDiv.addClass("row");
+      var $row = $("<div></div>");
+      $row.height((length / 3) - 4);
+      $row.addClass("row");
 
       for (var j = 0; j < 3; j++) {
-        var $cellDiv = $("<div></div>");
-        $cellDiv.width((length / 3) - 4);
-        $cellDiv.addClass("cell");
-
-        var $squareDiv = $("<div></div>");
-        $squareDiv.addClass("square");
-
-        $cellDiv.append($squareDiv);
-        $rowDiv.append($cellDiv);
+        this.generateCell($row, length, [i, j]);
       }
 
-      this.$el.append($rowDiv);
+      this.$el.append($row);
     }
   };
+
+  View.prototype.generateCell = function ($row, length, pos) {
+    var $cell = $("<div></div>");
+    $cell.width((length / 3) - 4);
+    $cell.addClass("cell");
+
+    var $square = $("<div><span></span></div>");
+    $square.data("pos", pos);
+    $square.css("line-height", $row.height() + "px");
+    $square.css("font-size", $row.height() / 1.75 + "px");
+    $square.addClass("square");
+
+    $cell.append($square);
+    $row.append($cell);
+  }
 })();
